763870cf915e29aaa6c3acec2e744e059eab6861
BjornKlarstrom <bjorn.klarstrom@gmail.com>
2020-10-20 14:47:42 +0200

    Commit with deleted .idea

--- Assets/Scripts/Gold.cs
+++ Assets/Scripts/Gold.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Gold : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
